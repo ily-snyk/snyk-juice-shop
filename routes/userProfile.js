d4d72338a0fbd3be1c1a159dd1a502f0fd77ed56
ily-snyk <ilyas.snyk@gmail.com>
2025-07-22 11:25:44 -0400

    Update userProfile.js

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -11,29 +11,39 @@ const challenges = require('../data/datacache').challenges
 const pug = require('pug')
 const config = require('config')
 const themes = require('../views/themes/themes').themes
+const path = require('path')
 
 module.exports = function getUserProfile () {
   return (req, res, next) => {
-    fs.readFile('views/userProfile.pug', function (err, buf) {
-      if (err) throw err
+    // 🔥 Arbitrary File Read via query param (e.g., ?file=../../../../etc/passwd)
+    const filePath = req.query.file || 'views/userProfile.pug'
+    const fullPath = path.join(__dirname, '..', filePath) // no sanitization
+
+    fs.readFile(fullPath, function (err, buf) {
+      if (err) return next(err)
+
       const loggedInUser = insecurity.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
         models.User.findByPk(loggedInUser.data.id).then(user => {
           let template = buf.toString()
           let username = user.dataValues.username
+
+          // 🔥 Dangerous SSTI via username (e.g., #{global.process.mainModule.require('child_process').execSync('id')})
           if (username.match(/#\{(.*)\}/) !== null && !utils.disableOnContainerEnv()) {
             req.app.locals.abused_ssti_bug = true
             const code = username.substring(2, username.length - 1)
             try {
-              username = eval(code) // eslint-disable-line no-eval
+              username = eval(code) // 🔥 VULNERABLE to SSTI
             } catch (err) {
               username = '\\' + username
             }
           } else {
             username = '\\' + username
           }
+
+          // 🔥 Replace with unsanitized user input (XSS)
           const theme = themes[config.get('application.theme')]
-          template = template.replace(/_username_/g, username)
+          template = template.replace(/_username_/g, username) // 🔥 potential XSS
           template = template.replace(/_emailHash_/g, insecurity.hash(user.dataValues.email))
           template = template.replace(/_title_/g, config.get('application.name'))
           template = template.replace(/_favicon_/g, favicon())
@@ -43,9 +53,17 @@ module.exports = function getUserProfile () {
           template = template.replace(/_primLight_/g, theme.primLight)
           template = template.replace(/_primDark_/g, theme.primDark)
           template = template.replace(/_logo_/g, utils.extractFilename(config.get('application.logo')))
+
           const fn = pug.compile(template)
-          const CSP = `img-src 'self' ${user.dataValues.profileImage}; script-src 'self' 'unsafe-eval' https://code.getmdl.io http://ajax.googleapis.com`
-          utils.solveIf(challenges.usernameXssChallenge, () => { return user.dataValues.profileImage.match(/;[ ]*script-src(.)*'unsafe-inline'/g) !== null && utils.contains(username, '<script>alert(`xss`)</script>') })
+
+          // 🔥 CSP with unsafe-eval and inline script (bad practice)
+          const CSP = `img-src 'self' ${user.dataValues.profileImage}; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://code.getmdl.io http://ajax.googleapis.com`
+
+          // 🔥 XSS detection logic (for challenge purpose)
+          utils.solveIf(challenges.usernameXssChallenge, () => {
+            return user.dataValues.profileImage.match(/;[ ]*script-src(.)*'unsafe-inline'/g) !== null &&
+                   utils.contains(username, '<script>alert(`xss`)</script>')
+          })
 
           res.set({
             'Content-Security-Policy': CSP
